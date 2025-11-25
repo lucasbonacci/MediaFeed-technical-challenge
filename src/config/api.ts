058606732ab681dfd8cae0ec394c0ea03f438a7a
lucasbonacci <lucas.bonacci96@gmail.com>
2025-11-25 01:28:55 -0300

    refactor: move api constants to config directory

---
+++ src/config/api.ts
@@ -0,0 +1,4 @@
+import Config from 'react-native-config';
+export const NEWS_API_KEY = Config.NEWS_API_KEY;
+export const NEWS_API_BASE_URL = Config.NEWS_API_BASE_URL;
+export const DEFAULT_PAGE_SIZE = 10;
